5eda9ef52cbcde6a8cf1d8851caee1dfbf06bfca
alkchofa <maxi-cantarell@hotmail.com>
2016-09-08 01:43:47 -0300

    Subo la primer version del script

---
+++ testHandShake.py
@@ -0,0 +1,9 @@
+import os
+import sys
+
+with open("Dicc.txt","w+") as arch:
+    for i in range(10000000):
+        arch.write("tele-"+str(i).zfill(7)+"\n")
+
+cadena="aircrack-ng -w Dicc.txt "+sys.argv[1]
+os.system(cadena)
